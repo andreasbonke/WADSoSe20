665b464287945d23be869aa306308664a04ca549
AndreasBonke <s0563482@htw-berlin.de>
2020-05-13 22:21:20 +0200

    add leflet map

--- adViz/src/js/drawOpenStreetMap.js
+++ adViz/src/js/drawOpenStreetMap.js
@@ -1,44 +1,26 @@
-        var map;
-        var layer_mapnik;
-        var layer_tah;
-        var layer_markers;
-
         function drawmap() {
             // Popup und Popuptext mit evtl. Grafik
             var popuptext = "<font color=\"black\"><b>HTW-Berlin<br>Wilhelminenhofstra&szlig;e 75 A<br>12459 Berlin</b><p><img src=\"img/contact_htw-berlin.jpg\" width=\"180\" height=\"113\"></p></font>";
 
-            OpenLayers.Lang.setCode('de');
-
             // Position und Zoomstufe der Karte
             var lon = 13.3916;
             var lat = 52.5172;
-            var zoom = 11;
+            var markerPos = {lat: 52.45722 , lon: 13.52694}
             
-            map = new OpenLayers.Map('map', {
-                projection: new OpenLayers.Projection("EPSG:900913"),
-                displayProjection: new OpenLayers.Projection("EPSG:4326"),
-                controls: [
-                    new OpenLayers.Control.Navigation(),
-                    new OpenLayers.Control.LayerSwitcher(),
-                    new OpenLayers.Control.PanZoomBar()],
-                maxExtent:
-                    new OpenLayers.Bounds(-20037508.34, -20037508.34,
-                        20037508.34, 20037508.34),
-                numZoomLevels: 18,
-                maxResolution: 156543,
-                units: 'meters'
-            });
+            var element = document.getElementById("map");
+ 
+            // initialize Leaflet
+      var map = L.map('map').setView({lon: lon, lat: lat}, 11);
 
-            layer_mapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
-            layer_markers = new OpenLayers.Layer.Markers("Address", {
-                projection: new OpenLayers.Projection("EPSG:4326"),
-                visibility: true, displayInLayerSwitcher: false
-            });
+      // add the OpenStreetMap tiles
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+      }).addTo(map);
 
-            map.addLayers([layer_mapnik, layer_markers]);
-            jumpTo(lon, lat, zoom);
+      // show the scale bar on the lower left corner
+      L.control.scale().addTo(map);
 
-            // Position des Markers
-            addMarker(layer_markers, 13.52694, 52.45722, popuptext);
-
+      // show a marker on the map
+      L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
         }
