8d148ffcf6a8f39080ad59c71e0bd0acb0e5cd98
AndreasBonke <s0563482@htw-berlin.de>
2020-05-14 02:17:49 +0200

    add some tiles

--- adViz/src/js/drawOpenStreetMap.js
+++ adViz/src/js/drawOpenStreetMap.js
@@ -1,24 +1,39 @@
+var map;
+var popup = L.popup();
+
 function drawmap() {
     
     // Position und Zoomstufe der Karte
     var lon = 13.3916;
     var lat = 52.5172;
-    var markerPos = { lat: 52.45722, lon: 13.52694 }
 
     var element = document.getElementById("map");
 
     // initialize Leaflet
-    var map = L.map('map').setView({ lon: lon, lat: lat }, 11);
+    map = L.map('map').setView({ lon: lon, lat: lat }, 11);
 
     // add the OpenStreetMap tiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
+    var osmde = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', { maxZoom: 19, attribution: 'Map data © OpenStreetMap and contributors CC-BY-SA' }).addTo(map);
+    var tonerLite = new L.StamenTileLayer("toner-lite");
+    var toner = new L.StamenTileLayer("toner");
+    var watercolor = new L.StamenTileLayer("watercolor");
+    var baseLayers = { "OSM": osmde, "Greyscale": tonerLite, "Black and White": toner, "Watercolor": watercolor}; L.control.layers(baseLayers).addTo(map);
 
     // show the scale bar on the lower left corner
     L.control.scale().addTo(map);
+   
+}
 
-    // show a marker on the map
-    L.marker(markerPos).bindPopup('<b>HTW-Berlin<br>Wilhelminenhofstra&szlig;e 75 A<br>12459 Berlin</b><p><img src=\"img/contact_htw-berlin.jpg\" width=\"180\" height=\"113\">').addTo(map);
+function showMarker(latitute, longitude){
+   
+    markerPos = [latitute, longitude];
+    var marker = L.marker(markerPos).addTo(map);    
+
+     popup
+     .setLatLng(markerPos)
+     .setContent("<b>HTW-Berlin<br>Wilhelminenhofstra&szlig;e 75 A<br>12459 Berlin</b><p><img src=\"img/contact_htw-berlin.jpg\" width=\"180\" height=\"113\">");
+     
+    marker.bindPopup(popup).openPopup();
+
+    map.setView(markerPos,18);
 }
