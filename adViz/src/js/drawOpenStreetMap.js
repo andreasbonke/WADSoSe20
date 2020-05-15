2baf549a6268416366d98d27b482259236501eb6
AndreasBonke <s0563482@htw-berlin.de>
2020-05-15 12:06:08 +0200

    update

--- adViz/src/js/drawOpenStreetMap.js
+++ adViz/src/js/drawOpenStreetMap.js
@@ -1,23 +1,21 @@
-var map;
-var popup = L.popup();
+let map;
+let popup = L.popup();
 
 function drawmap() {
     
     // Position und Zoomstufe der Karte
-    var lon = 13.3916;
-    var lat = 52.5172;
-
-    var element = document.getElementById("map");
+    const lon = 13.3916;
+    const lat = 52.5172;
 
     // initialize Leaflet
     map = L.map('map').setView({ lon: lon, lat: lat }, 11);
 
     // add the OpenStreetMap tiles
-    var osmde = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', { maxZoom: 19, attribution: 'Map data © OpenStreetMap and contributors CC-BY-SA' }).addTo(map);
-    var tonerLite = new L.StamenTileLayer("toner-lite");
-    var toner = new L.StamenTileLayer("toner");
-    var watercolor = new L.StamenTileLayer("watercolor");
-    var baseLayers = { "OSM": osmde, "Greyscale": tonerLite, "Black and White": toner, "Watercolor": watercolor}; L.control.layers(baseLayers).addTo(map);
+    let osmde = L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', { maxZoom: 19, attribution: 'Map data © OpenStreetMap and contributors CC-BY-SA' }).addTo(map);
+    let tonerLite = new L.StamenTileLayer("toner-lite");
+    let toner = new L.StamenTileLayer("toner");
+    let watercolor = new L.StamenTileLayer("watercolor");
+    let baseLayers = { "OSM": osmde, "Greyscale": tonerLite, "Black and White": toner, "Watercolor": watercolor}; L.control.layers(baseLayers).addTo(map);
 
     // show the scale bar on the lower left corner
     L.control.scale().addTo(map);
@@ -26,8 +24,8 @@ function drawmap() {
 
 function showMarker(latitute, longitude){
    
-    markerPos = [latitute, longitude];
-    var marker = L.marker(markerPos).addTo(map);    
+    let markerPos = [latitute, longitude];
+    let marker = L.marker(markerPos).addTo(map);    
 
      popup
      .setLatLng(markerPos)
