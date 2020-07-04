cc12609b0d1f6ed3398d44d0302700bfc9b600f9
AndreasBonke <s0563482@htw-berlin.de>
2020-07-04 20:11:34 +0200

    fixed Contact Info Window

--- adviz/src/components/ContactInfoWindow.js
+++ adviz/src/components/ContactInfoWindow.js
@@ -1,33 +1,28 @@
 import React from 'react'
 
-const ContactInfoWindow = ({ contacts, deleteContact, handleClose }) => {
-
-    console.log(contacts)
+const ContactInfoWindow = ({ contact, deleteContact, handleClose }) => {
 
     return (
-        <div>
-            {contacts.map(contact => (
-                <div key={contact.id} className="modal" id="modalAddressInfo">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <span className="close" id="closeContactForm" onClick={() => { handleClose() }}>&times;</span>
-                            <h2>Address</h2>
-                        </div>
-                        <div className="container">
-                            <div>Forname: {contact.forname}</div>
-                            <div>Name: {contact.name}</div>
-                            <div>Street: {contact.street}</div>
-                            <div>Postcode: {contact.postId}</div>
-                            <div>Town: {contact.town}</div>
-                            <div>Country: {contact.country}</div>
-                            <div>Private: </div>
-                            <input type="checkbox" id="updateAddressCheck" checked={contact.isPrivate} disabled />
-                            <button className="form-button" onClick={() => { deleteContact(contact.id) }}>Delete</button>
-                        </div>
-                    </div>
+        <div className="modal" id="modalAddressInfo">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <span className="close" id="closeContactForm" onClick={() => { handleClose() }}>&times;</span>
+                    <h2>Address</h2>
                 </div>
-            ))}
+                <div className="container">
+                    <div>Forname: {contact.forname}</div>
+                    <div>Name: {contact.name}</div>
+                    <div>Street: {contact.street}</div>
+                    <div>Postcode: {contact.postId}</div>
+                    <div>Town: {contact.town}</div>
+                    <div>Country: {contact.country}</div>
+                    <div>Private: </div>
+                    <input type="checkbox" id="updateAddressCheck" checked={contact.isPrivate} disabled />
+                    <button className="form-button" onClick={() => { deleteContact(contact.id) }}>Delete</button>
+                </div>
+            </div>
         </div>
+
     )
 }
 
--- adviz/src/components/Main.js
+++ adviz/src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ContactList from './ContactList'
 import Footer from './Footer'
 import MapView from './MapView';
@@ -10,13 +10,47 @@ class Main extends Component {
 
     state = {
         contacts: [
-            { forname: 'Andreas', name: 'Bonke', street: 'Am Krusenick', postId: '12555', town: 'Berlin', country: 'Deutschland', isPrivate: false, id: 1 },
-            { forname: 'Julia', name: 'Koldewitz', street: 'Am Krusenick', postId: '12555', town: 'Berlin', country: 'Deutschland', isPrivate: false, id: 2 },
-            { forname: 'Marvin', name: 'Rausch', street: 'Gaillardstrasse', postId: '13187', town: 'Berlin', country: 'Deutschland', isPrivate: true, id: 3 },
+            {
+                forname: 'Andreas',
+                name: 'Bonke',
+                street: 'Am Krusenick',
+                postId: '12555',
+                town: 'Berlin',
+                country: 'Deutschland',
+                isPrivate: false,
+                id: 1
+            },
+            {
+                forname: 'Julia',
+                name: 'Koldewitz',
+                street: 'Am Krusenick',
+                postId: '12555',
+                town: 'Berlin',
+                country: 'Deutschland',
+                isPrivate: false,
+                id: 2
+            },
+            {
+                forname: 'Marvin',
+                name: 'Rausch',
+                street: 'Gaillardstrasse',
+                postId: '13187',
+                town: 'Berlin',
+                country: 'Deutschland',
+                isPrivate: true,
+                id: 3
+            },
         ],
-        show: false,
+        actualContact: {
+            forname: "",
+            name: "",
+            street: "",
+            postId: "",
+            town: "",
+            country: "",
+            isPrivate: "",
+        }
     }
-
 
 
     addContact = (contact) => {
@@ -30,17 +64,23 @@ class Main extends Component {
     }
 
     deleteContact = (id) => {
-        // console.log(id);
         let contacts = this.state.contacts.filter(contact => {
             return contact.id !== id
         });
         this.setState({
             contacts: contacts
         });
+        document.querySelector("#modalAddressInfo").hidden = true;
     }
 
     showContactInfo = (id) => {
-        console.log(id)
+        let contact = this.state.contacts.find(contact => {
+            return contact.id === id
+        });
+
+        this.setState({
+            actualContact: contact
+        });
         document.querySelector("#modalAddressInfo").hidden = false;
     }
 
@@ -50,13 +90,14 @@ class Main extends Component {
 
     // Methode wird ausgeführt nachdem die Komponenten in das DOM gerendert wurde
     componentDidMount() {
-        console.log(this.state.actualContact);
+        //console.log(this.state.actualContact);
         document.querySelector("#modalAddress").hidden = true;
+        document.querySelector("#modalAddressInfo").hidden = true;
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('component updated');
-        console.log(prevProps, prevState)
+        //console.log('component updated');
+        //console.log(prevProps, prevState)
     }
 
     componentWillUnmount() {
@@ -66,11 +107,12 @@ class Main extends Component {
     render() {
         return (
             <div id="main">
-                <MapView />
-                <ContactInfoWindow contacts={this.state.contacts} deleteContact={this.deleteContact} handleClose={this.handleClose} />
-                <ContactList contacts={this.state.contacts} showContactInfo={this.showContactInfo} />
-                <AddContactForm addContact={this.addContact} />
-                <Footer />
+                <MapView/>
+                <ContactList contacts={this.state.contacts} showContactInfo={this.showContactInfo}/>
+                <ContactInfoWindow contact={this.state.actualContact} deleteContact={this.deleteContact}
+                                   handleClose={this.handleClose}/>
+                <AddContactForm addContact={this.addContact}/>
+                <Footer/>
             </div>
         )
     }
